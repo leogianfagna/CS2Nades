07f4ca1862452536a20a915a3c2b680a2a1cb1db
Léo <leogianfagna@gmail.com>
2025-02-13 18:01:53 -0300

    Updates

---
+++ data/nades.jsx
@@ -0,0 +1,40 @@
+const nades = [
+  {
+    map: "mirage",
+    type: "smokes",
+    local: "🪟 Janela",
+    throw: "D + Jump and Throw",
+    image1: "pixels/mirage-janela-spot.png",
+    image2: "pixels/mirage-janela-pixel.png",
+    image3: ""
+  },
+  {
+    map: "mirage",
+    type: "smokes",
+    local: "📦 CT",
+    throw: "Jump and Throw",
+    image1: "pixels/mirage-ct-spot.png",
+    image2: "pixels/mirage-ct-pixel.png",
+    image3: ""
+  },
+  {
+    map: "mirage",
+    type: "smokes",
+    local: "🤯 Cabecinha",
+    throw: "Clique esquerdo",
+    image1: "pixels/mirage-cabecinha-spot.png",
+    image2: "pixels/mirage-cabecinha-pixel.png",
+    image3: ""
+  },
+  {
+    map: "mirage",
+    type: "smokes",
+    local: "🐊 Jungle",
+    throw: "Jump and Throw",
+    image1: "pixels/mirage-jungle-spot.png",
+    image2: "pixels/mirage-jungle-pixel.png",
+    image3: ""
+  }
+];
+
+export { nades };
