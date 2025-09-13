5baabbd59d0416562394b5d9f2b511ef9e263c9b
Léo <leogianfagna@gmail.com>
2025-09-13 11:07:03 -0300

    style: Cabeçalho no container da imagem

--- src/components/features/RenderingImages/NadeImage.jsx
+++ src/components/features/RenderingImages/NadeImage.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { FilterContext } from "/src/context/FilterContext";
-import styles from "./NadeImage.module.css";
+import styles from "./NadeImage.module.scss";
 
 const NadeImage = () => {
   const { nade } = useContext(FilterContext);
@@ -11,7 +11,7 @@ const NadeImage = () => {
         src={nade.spot_image}
         alt="Pixel da granada"
         className={`${styles["nade-image"]} ${styles["left"]}`}
-      ></img>
+      />
     );
   };
 
@@ -22,21 +22,27 @@ const NadeImage = () => {
           src={nade.pixel_image}
           alt="Pixel da granada"
           className={`${styles["nade-image"]} ${styles["right"]}`}
-        ></img>
+        />
         <div className="overlay-text">{nade?.throw ?? ""}</div>
       </>
     );
   };
 
-  // To-do: customizar mais a imagem, está não centralizada, sem efeitos, etc
   return (
     <div className={styles["container-image"]}>
-      {nade && (
-        <>
-          <div className={styles["spot-image"]}>{renderLeftImage()}</div>
-          <div className={styles["spot-image"]}>{renderRightImage()}</div>
-        </>
-      )}
+      <header>
+        <span>Posição: {nade.standing ?? "N/A"}</span>
+        <span>Arremesso: {nade.throw ?? "N/A"}</span>
+        <span>Movimento: {nade.movement ?? "N/A"}</span>
+      </header>
+      <div className={styles["image-grid"]}>
+        {nade && (
+          <>
+            <div className={styles["spot-image"]}>{renderLeftImage()}</div>
+            <div className={styles["spot-image"]}>{renderRightImage()}</div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
