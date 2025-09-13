45b21699e894d574189d1a05a80da5adbcaec6e3
Léo <leogianfagna@gmail.com>
2025-09-13 11:18:49 -0300

    style: Customização do ícone de nade no mapa

--- src/components/commons/Nade.jsx
+++ src/components/commons/Nade.jsx
@@ -1,23 +1,17 @@
 import { useContext } from "react";
 import { FilterContext } from "/src/context/FilterContext";
+import styles from "./Nade.module.scss";
 
 const Nade = ({ pos, nade }) => {
   const { setNade } = useContext(FilterContext);
   const [x, y] = pos;
 
   return (
-    <button
-      style={{
-        borderRadius: "30px",
-        position: "absolute",
-        right: `${x}%`,
-        top: `${y}%`,
-        opacity: "0.3",
-      }}
+    <div
+      style={{ right: `${x}%`, top: `${y}%` }}
+      className={styles[nade.team]}
       onClick={() => setNade(nade)}
-    >
-      {nade.type}
-    </button>
+    />
   );
 };
 
